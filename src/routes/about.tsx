8760429510181855d30c5c884e7f58b3ee8c2555
Manolis <moneymanolis@protonmail.com>
2024-09-26 19:05:44 +0200

    test commit to check nano-staged works!

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+// TODO: Add about component
 export const Route = createFileRoute('/about')({
-  component: () => <div>Hello from the /about route!</div>
+  component: () => <div>Hello from the /about route!</div>,
 })
