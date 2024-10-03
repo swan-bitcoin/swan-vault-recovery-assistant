5f26a6bfe9205ae8bf1cc2ce58666cff5734aa34
Jesse Frush <jesse@swanbitcoin.com>
2024-10-03 10:17:00 -0700

    add specta for strong typing, convert Rust backend to stateless, simplify backend

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,57 @@
-import { invoke } from "@tauri-apps/api/core";
+// import { invoke } from "@tauri-apps/api/core";
+import { commands, TempuraError } from "./bindings";
 
-let descriptorInput: HTMLInputElement | null;
+let receiveInput: HTMLInputElement | null;
+let changeInput: HTMLInputElement | null;
 let balanceMessage: HTMLElement | null;
 
-async function setWallet() {
-  if (balanceMessage && descriptorInput) {
-    console.log(descriptorInput.value);
-    await invoke("set_wallet", {
-      receive: descriptorInput.value,
-      change: "",
-    }).catch(() => {
-      if (balanceMessage) {
-        balanceMessage.textContent = "Invalid descriptor!";
-      }
-    });
-    balanceMessage.textContent = "Wallet set!";
-  }
-}
+// async function setWallet() {
+//   if (balanceMessage && receiveInput) {
+//     const change = changeInput?.value;
+//     await commands.setWallet(receiveInput.value, change).catch((e) => {
+//       if (balanceMessage) {
+//         balanceMessage.textContent = e.message;
+//       }
+//     });
+//     balanceMessage.textContent = "Wallet set!";
+//   }
+// }
 
 async function fetchBalance() {
-  if (balanceMessage && descriptorInput) {
-    const balance = (await invoke("fetch_balance")) as { confirmed: number };
-    balanceMessage.textContent = balance.confirmed + " sats";
+  const recv = receiveInput?.value;
+  if (!recv) {
+    if (balanceMessage) {
+      balanceMessage.textContent = "A valid receive descriptor is required";
+    }
+    return;
+  }
+
+  if (balanceMessage) {
+    const change = changeInput?.value ?? null;
+    balanceMessage.textContent = "Please wait...";
+    try {
+      const balance = await commands.fetchBalance(recv, change);
+      balanceMessage.textContent = balance.confirmed + " sats";
+    } catch (e: unknown) {
+      // TODO: this error strategy is causing the shape to change depending on the error.
+      console.log(e);
+      if (e instanceof Error) {
+        balanceMessage!.textContent = e.message;
+      } else {
+        balanceMessage!.textContent = "An unknown error occurred";
+      }
+    }
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  descriptorInput = document.querySelector("#desciptor-input");
+  receiveInput = document.querySelector("#receive-input");
+  changeInput = document.querySelector("#change-input");
   balanceMessage = document.querySelector("#balance-msg");
   document
     .querySelector("#descriptor-form")
     ?.addEventListener("submit", (e) => {
       e.preventDefault();
-      setWallet();
+      fetchBalance();
     });
-  document.querySelector("#balance-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetchBalance();
-  });
 });
