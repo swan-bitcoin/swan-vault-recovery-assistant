9e865634cfa1d0427901a2885ea4e9fed9b32d02
Brandon Black <freedom@reardencode.com>
2024-10-21 16:41:19 -0700

    build: get HWI binary in pnpm postinstall not GHA
    * Add a TS script to fetch HWI into the expected location
    * Run ^ fetch script in pnpm postinstall
    * Remove GHA to fetch HWI

---
+++ scripts/fetch-hwi.ts
@@ -0,0 +1,63 @@
+import { Readable } from 'stream'
+import { execSync } from 'child_process'
+import * as unzip from 'unzip-stream'
+import * as fs from 'fs/promises'
+import { createWriteStream } from 'fs'
+import * as path from 'path'
+import * as os from 'os'
+import * as tar from 'tar'
+
+const HWI_VERSION = '3.1.0'
+const URL_BASE = `https://github.com/bitcoin-core/HWI/releases/download/${HWI_VERSION}/hwi-${HWI_VERSION}`
+
+function makeUrl(platform: string, triple: string) {
+  const arch = triple.split('-')[0]
+  if (platform === 'win32') return `${URL_BASE}-windows-${arch}.zip`
+  let hwiPlatform: string
+  switch (platform) {
+    case 'linux':
+      hwiPlatform = 'linux'
+      break
+    case 'darwin':
+      hwiPlatform = 'mac'
+      break
+    default:
+      throw new Error(`Unexpected platform: ${platform}`)
+  }
+  return `${URL_BASE}-${hwiPlatform}-${arch}.tar.gz`
+}
+
+function extractor(platform: string, tmpdir: string) {
+  if (platform !== 'win32') return tar.x({ cwd: tmpdir }, ['hwi'])
+  return unzip.Parse().on('entry', function (entry) {
+    if (entry.path === 'hwi.exe') {
+      entry.pipe(createWriteStream(path.join(tmpdir, 'hwi')))
+    } else {
+      entry.autodrain()
+    }
+  })
+}
+
+async function main() {
+  const data = execSync('rustc -vV').toString()
+  const triple = data.match(/host: ([^\s]+)/)[1]
+  const platform = os.platform()
+  const url = makeUrl(platform, triple) // make url ASAP to fail fast on unsupported platform
+  const hwiFile = path.join('src-tauri', `hwi-${triple}${platform === 'win32' ? '.exe' : ''}`)
+  try {
+    await fs.access(hwiFile)
+    return // HWI already in the expected place
+  } catch {
+    // Need to get HWI
+  }
+  // tmpdir in destination so rename is atomic and never cross-device
+  const tmpdir = await fs.mkdtemp(path.join('src-tauri', '.tmphwi-'))
+  try {
+    const hwiStream = await fetch(url).then((r) => Readable.fromWeb(r.body as any).pipe(extractor(platform, tmpdir)))
+    await new Promise((resolve) => hwiStream.on('finish', resolve))
+    await fs.rename(path.join(tmpdir, 'hwi'), hwiFile)
+  } finally {
+    await fs.rm(tmpdir, { recursive: true, force: true })
+  }
+}
+main()
