1360d9b0bafbb6fda79be0d6f6e765707d114eb9
Jesse Frush <jesse@swanbitcoin.com>
2024-10-29 12:03:48 -0700

    add selenium webdriver and config necessary for e2e tests, proof of test

---
+++ test/e2e.test.ts
@@ -0,0 +1,14 @@
+import os from 'os'
+import path from 'path'
+import { spawn, spawnSync } from 'child_process'
+import { Builder, By, Capabilities } from 'selenium-webdriver'
+import { beforeAll, afterAll, describe, expect, it } from 'vitest'
+
+import { driver } from './setup.integration'
+
+describe('e2e', function () {
+  it('should open the window', async function () {
+    const test = await driver.findElement(By.id('temporary-message')).getText()
+    expect(test).toMatch(/^Hi there!/)
+  })
+})
