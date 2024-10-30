46107b2847d6b16d7c8b4192916548b9dde1570f
Jesse Frush <jesse@swanbitcoin.com>
2024-10-30 09:50:44 -0700

    scenario tests conditionally rebuild the application when source or config files have changed

--- test/setup.scenario.ts
+++ test/setup.scenario.ts
@@ -1,18 +1,81 @@
 import os from 'os'
+import fs from 'fs'
 import path from 'path'
-import { afterAll, beforeAll } from 'vitest'
+import * as crypto from 'crypto'
+import { afterAll, beforeAll, should } from 'vitest'
 import { spawn, spawnSync } from 'child_process'
 import { Builder, By, Capabilities } from 'selenium-webdriver'
 
 export let driver
 let tauriDriver
+
 const application = path.resolve(__dirname, '..', 'src-tauri', 'target', 'release', 'tempura')
+const hashFile = path.resolve(__dirname, '..', 'src-tauri', 'target', 'tempura-hashfile')
+
+// a list of files and directories to hash to determine when we should build
+const tauriDir = path.resolve(__dirname, '..', 'src-tauri')
+const sourcePaths = [
+  path.resolve(__dirname, '..', 'dist'),
+  path.resolve(__dirname, '..', 'src'),
+  path.resolve(__dirname, '..', 'index.html'),
+  path.join(tauriDir, 'Cargo.lock'),
+  path.join(tauriDir, 'Cargo.toml'),
+  path.join(tauriDir, 'capabilities'),
+  path.join(tauriDir, 'gen'),
+  path.join(tauriDir, 'icons'),
+  path.join(tauriDir, 'src'),
+  path.join(tauriDir, 'tauri.conf.json'),
+]
+
+/**
+ * recursively hashes all files in the given paths
+ */
+function hashFiles(paths: string[]): string {
+  const hash = crypto.createHash('sha256')
+
+  function hashPath(p: string) {
+    const stat = fs.statSync(p)
+    if (stat.isFile()) {
+      const content = fs.readFileSync(p)
+      hash.update(content)
+      return
+    }
+
+    if (stat.isDirectory()) {
+      const files = fs.readdirSync(p)
+      files.forEach((file) => {
+        const fullPath = path.join(p, file)
+        hashPath(fullPath)
+      })
+    }
+  }
+
+  paths.forEach((p) => {
+    hashPath(p)
+  })
+
+  return hash.digest('hex')
+}
 
 beforeAll(async () => {
-  // TODO
-  // this causes it to rebuild release -every- time, which is super slow.
-  // consider checking for binary exist, which could lead to unexpected results locally
-  //   await spawnSync('pnpm', ['tauri', 'build'], { stdio: 'inherit' })
+  // perform a build if the hash file doesn't exist (first run) or if the source files have changed
+  let currentHash
+  let shouldBuild = !fs.existsSync(hashFile)
+  if (!shouldBuild) {
+    const previousHash = fs.readFileSync(hashFile, 'utf8')
+
+    // gen the the frontend 'dist' contents and hash the source directories
+    await spawnSync('pnpm', ['build'], { stdio: 'inherit' })
+    currentHash = hashFiles(sourcePaths)
+    shouldBuild = currentHash !== previousHash
+  }
+
+  if (shouldBuild) {
+    await spawnSync('pnpm', ['tauri', 'build', '--no-bundle'], { stdio: 'inherit' })
+    fs.writeFileSync(hashFile, currentHash ?? hashFiles(sourcePaths))
+  } else {
+    console.log('build will be skipped because source files have not changed')
+  }
 
   tauriDriver = spawn(path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'), [], {
     // stdio: ['ignore', process.stdout, process.stderr],
