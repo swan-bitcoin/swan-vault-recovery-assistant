663f57eaefab99c6197247c6fb200f7971797868
Manolis Mandrapilias <70536101+moneymanolis@users.noreply.github.com>
2024-11-04 22:25:15 +0100

    Only warning for change descriptors (#55)
    * only show a warning in the shrimpy bubble when user inputs a change descriptor
    
    * make feedback for invalid wallet config less jumpy
    
    * adjust tooltip text --> checksum not needed for tempura
    
    * move the checked property from testnet to bitcoin network by default
    
    ---------
    
    Co-authored-by: Jesse Frush <jesse@swanbitcoin.com>

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -320,12 +320,7 @@ const validateDescriptor = async () => {
     DOM.tempMessage.textContent = 'Wallet configuration is missing!'
     DOM.receiveInput.classList.add('textarea-error')
     DOM.receiveInput.classList.remove('textarea-success')
-    return false
-  }
-  if (isChangeDescriptor(descriptor)) {
-    DOM.tempMessage.textContent = 'You are trying to use a change descriptor! Please provide a receive descriptor instead.'
-    DOM.receiveInput.classList.add('textarea-error')
-    DOM.receiveInput.classList.remove('textarea-success')
+    DOM.receiveInput.classList.remove('textarea-warning')
     return false
   }
   try {
@@ -335,19 +330,32 @@ const validateDescriptor = async () => {
         'Descriptor is fine but it is for the wrong network. Switch to Advanced Mode to change the network!'
       DOM.receiveInput.classList.add('textarea-error')
       DOM.receiveInput.classList.remove('textarea-success')
+      DOM.receiveInput.classList.remove('textarea-warning')
       return false
+    }
+    if (isChangeDescriptor(descriptor)) {
+      DOM.tempMessage.textContent =
+        'You seem to be using a change descriptor for your wallet configuration. This may limit wallet functionality, such as showing only a partial balance instead of the full wallet balance.'
+      DOM.receiveInput.classList.add('textarea-warning')
+      DOM.receiveInput.classList.remove('textarea-success')
+      DOM.receiveInput.classList.remove('textarea-error')
+      recoveryOptionsCard.classList.remove('hidden')
+      standardWalletActions.classList.remove('hidden')
+      return true
     }
     DOM.receiveInput.classList.add('textarea-success')
     DOM.receiveInput.classList.remove('textarea-error')
+    DOM.receiveInput.classList.remove('textarea-warning')
     DOM.tempMessage.textContent =
       'Your wallet configuration is valid. You can now fetch your balance and perform other actions.'
     recoveryOptionsCard.classList.remove('hidden')
     standardWalletActions.classList.remove('hidden')
     return true
-  } catch (error) {
-    handleError(error)
+  } catch {
+    DOM.tempMessage.textContent = 'Invalid wallet configuration!'
     DOM.receiveInput.classList.add('textarea-error')
     DOM.receiveInput.classList.remove('textarea-success')
+    DOM.receiveInput.classList.remove('textarea-warning')
     return false
   }
 }
