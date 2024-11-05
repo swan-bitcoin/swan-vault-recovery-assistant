3c155cf1c2915eb039d2b9c181f7dd0c9f2c6792
Manolis <moneymanolis@protonmail.com>
2024-11-05 19:45:28 +0100

    add icons

---
+++ src/icons/selfTransferIcon.ts
@@ -0,0 +1,9 @@
+export const selfTransferIcon = `
+<div class="tooltip tooltip-info tooltip-right" data-tip="Self-transfer">
+  <div class="text-info">
+    <svg xmlns="http://www.w3.org/2000/svg" x-bind:width="size" x-bind:height="size" viewBox="0 0 24 24" fill="none" stroke="currentColor" x-bind:stroke-width="stroke" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+      <path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1"></path>
+    </svg>
+  </div>
+</div>
+`
---
+++ src/icons/sentIcon.ts
@@ -0,0 +1,9 @@
+export const sentIcon = `
+<div class="tooltip tooltip-warning tooltip-right" data-tip="Sent">
+  <div class="text-warning">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" width="24" height="24">
+      <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4"></path>
+    </svg>
+  </div>
+</div>
+`
