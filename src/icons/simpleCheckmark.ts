e2c1809ade0f7192a655a77d83d6029902964625
Manolis <moneymanolis@protonmail.com>
2024-11-07 10:23:14 +0100

    add simpleCheckmark icon

---
+++ src/icons/simpleCheckmark.ts
@@ -0,0 +1,5 @@
+export const simpleCheckmark = `
+<svg class="inline w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M5 12l5 5L20 7"></path>
+</svg>
+`
