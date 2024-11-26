70b15af413f68d64e8574d598ac0212d02ee4f91
Manolis <moneymanolis@protonmail.com>
2024-11-26 18:17:52 +0100

    add utilities for transactions

---
+++ src/utilities/getFirstTransaction.ts
@@ -0,0 +1,16 @@
+import { Transaction } from '@/bindings'
+
+/**
+ * Finds the first confirmed transaction.
+ */
+export const getFirstTransaction = (transactions: Transaction[]): Transaction | null => {
+  const confirmedTransactions = transactions.filter((tx) => tx.confirmation_height !== null)
+
+  if (confirmedTransactions.length === 0) {
+    return null
+  }
+
+  return confirmedTransactions.reduce((firstTx, currentTx) => {
+    return currentTx.confirmation_height < firstTx.confirmation_height ? currentTx : firstTx
+  })
+}
