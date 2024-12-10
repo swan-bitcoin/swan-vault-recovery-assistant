600f7f1610ef98ce3b1c429934fcee4d142e609e
Manolis Mandrapilias <70536101+moneymanolis@users.noreply.github.com>
2024-12-10 21:05:22 +0100

    Fix transactions list logic (#110)

--- src/components/Transactions.ts
+++ src/components/Transactions.ts
@@ -4,8 +4,26 @@ import { receivedIcon, sentIcon, selfTransferIcon } from '../icons'
 
 const TxRow = (transaction: Transaction) => {
   // Determine the type of transaction
-  const transactionType =
-    transaction.sent === transaction.fee ? 'selfTransfer' : Number(transaction.received) > 0 ? 'received' : 'sent'
+  let transactionType: 'selfTransfer' | 'sent' | 'received'
+  if (Number(transaction.sent) === Number(transaction.received) + Number(transaction.fee)) {
+    // Self-transfer: Everything that's being sent goes to fees and change
+    transactionType = 'selfTransfer'
+  } else if (Number(transaction.sent) > 0 && Number(transaction.received) > 0) {
+    // Sent transaction with change output
+    transactionType = 'sent'
+  } else if (Number(transaction.received) > 0) {
+    transactionType = 'received'
+  } else {
+    // Sent transaction without change output
+    transactionType = 'sent'
+  }
+
+  const amount =
+    transactionType === 'sent'
+      ? `-${Sats(Number(transaction.sent) - Number(transaction.received))}`
+      : transactionType === 'received'
+        ? `+${Sats(Number(transaction.received))}`
+        : `-${Sats(Number(transaction.fee))}` // selfTransfer reduces balance amount just by the fee
 
   return `
       <tr>
@@ -14,9 +32,7 @@ const TxRow = (transaction: Transaction) => {
         </td>
         <td>${transaction.txid}</td>
         <td>${CopyButtonXs(transaction.txid)}</td>
-        <td>
-          ${transactionType === 'selfTransfer' ? '' : transactionType === 'sent' ? Sats(transaction.sent) : Sats(transaction.received)}
-        </td>
+        <td>${amount}</td>
         <td>${Sats(transaction.fee)}</td>
         <td>${transaction.confirmation_height || 'Unconfirmed'}</td>
       </tr>
